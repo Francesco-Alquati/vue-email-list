ac3576f916bfb2d90df79350cf292a99abafea34
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-25 17:25:30 +0200

    [UPD] Got response from API to create one random email

--- js/script.js
+++ js/script.js
@@ -3,14 +3,19 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
-        
+        email: null,
         
       }
     },
     created (){
-
+        this.generateEmails();
     },
     methods: {
+        generateEmails(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                this.email = response.data.response
+            });
+        }
 
     }
   }).mount('#app')
